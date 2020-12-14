c30e31c2cfac48f3499fea62aa2637037737b4d1
ChloeArd <chloe.ardoise@gmail.com>
2020-12-14 10:00:41 +0100

    Exercice 79

--- exo.js
+++ exo.js
@@ -1,3 +1,24 @@
 let pomme = "Elle est belle ma pomme !";
 
-console.log(pomme.lastIndexOf('m'));
+let indexPomme = document.getElementById("index-pomme");
+indexPomme.innerHTML = pomme.lastIndexOf("pomme");
+
+let lastIndexM = document.getElementById("last-index-m");
+lastIndexM.innerHTML = pomme.lastIndexOf('m');
+
+if(pomme.startsWith("Elle")) {
+    console.log(pomme + " Commence par elle");
+}
+
+if (pomme.endsWith("!")) {
+    console.log(pomme + " Se termine par un point d'exclamation")
+}
+
+document.getElementById("pomme").innerHTML = pomme.slice(18, 23);
+
+
+let oneLine = document.getElementById("one-line");
+
+for (let i = 0; i < pomme.length; i++) {
+    oneLine.innerHTML = pomme.split("");
+}
